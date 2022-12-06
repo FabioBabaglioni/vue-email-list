a090b6e62c2dc2ea5206312bfb6c8265af605c7b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-06 15:57:31 +0100

    AGgiunta ciclo per stampare le 10 mail

--- js/main.js
+++ js/main.js
@@ -9,15 +9,23 @@ createApp({
     }
   },
 
-  methods : {
-  },
+  methods : { 
+
+        randoMail(){
+            for(let i = 0; i < 11; i++){
+
+            }
+        }
+
+    },
 
   mounted(){
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
         .then((result) => {
             this.mail = result.data.response
+            console.log(result.data.response)
         });
     
-  }
+         }
 
 }).mount('#app')
