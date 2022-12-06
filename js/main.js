f4960f860b390909cc634084abf17c381d7017bc
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-06 15:44:26 +0100

    Aggiunta una mail alla lista

--- js/main.js
+++ js/main.js
@@ -4,12 +4,20 @@ createApp({
   data() {
     return {
 
-      message: 'Hello Vue!',
+      mail: '',
 
     }
   },
 
   methods : {
+  },
+
+  mounted(){
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+            this.mail = result.data.response
+        });
+    
   }
-  
+
 }).mount('#app')
