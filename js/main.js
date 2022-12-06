1959902e08eb955a787312783fb67ee7cfba619a
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-06 16:21:46 +0100

    Esercizio completo

--- js/main.js
+++ js/main.js
@@ -6,26 +6,41 @@ createApp({
 
       mail: '',
 
+    //   mi creo un array vuoto che posso popolare
+      eMail : [],
+
     }
   },
 
   methods : { 
 
-        randoMail(){
-            for(let i = 0; i < 11; i++){
+    // creo randoEmail per popolare l'array
+    randoMail(){
+        // creo nil ciclo per stamparmi la mail 10 volte
+        for(let i = 0; i < 10; i++){
 
-            }
+            // mi richiamo axios
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+
+            // uso una arrow function per poter uscire da questo scop
+            .then((result) => {
+            
+            // richiamo le mail generate automaticamente
+            this.mail = result.data.response
+            
+            // pusho la mail creato nell'array
+            this.eMail.push(this.mail)
+            });
         }
+    }
 
     },
 
   mounted(){
-        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-        .then((result) => {
-            this.mail = result.data.response
-            console.log(result.data.response)
-        });
-    
-         }
+
+    // faccio partire la funzione in modo asincrono rispetto al caricamento della pèagina 
+    this.randoMail()
+
+    }
 
 }).mount('#app')
